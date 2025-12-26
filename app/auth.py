d7a5adda6a08e2d95966a1c49634e07105b9a263
weinibuliu <weinibuliu@outlook.com>
2025-12-26 22:30:09 +0800

    fix: root user

--- app/auth.py
+++ app/auth.py
@@ -13,7 +13,7 @@ def check_root(request: Request, auth: str | None) -> None:
         raise HTTPException(status_code=403, detail="Unauthorized")
 
     with next(get_session()) as session:
-        user = session.get(User, 10001)
+        user = session.get(User, 1)  # user 1 = root user
         if not (user and user.auth == auth):
             rate_limiter.record_failed_attempt(client_ip)
             raise HTTPException(status_code=403, detail="Unauthorized")
